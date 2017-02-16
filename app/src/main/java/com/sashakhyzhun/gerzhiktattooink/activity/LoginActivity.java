9c53ca118dcbb9b7c8b51b1535daaab7db7afd89
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-02-17 00:52:28 +0200

    added signIn with google.

--- app/src/main/java/com/sashakhyzhun/gerzhiktattooink/activity/LoginActivity.java
+++ app/src/main/java/com/sashakhyzhun/gerzhiktattooink/activity/LoginActivity.java
@@ -18,6 +18,12 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.sashakhyzhun.gerzhiktattooink.R;
 import com.sashakhyzhun.gerzhiktattooink.utils.SessionManager;
 
@@ -35,9 +41,13 @@ import java.util.Arrays;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "LoginActivity";
-    private String user_email, path_to_pic, name, fb_id;
-    private LoginButton loginButton;
+    private static final int RC_SIGN_IN = 228;
+    private String userEmail, userPicture, userName, userID;
+    private LoginButton btnLoginFacebook;
+    private SignInButton btnLoginGoogle;
+
     private CallbackManager callbackManager;
+    private GoogleApiClient mGoogleApiClient;
     private AccessTokenTracker accessTokenTracker;
 
     @Override
@@ -48,64 +58,98 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
         /************************ for API >= 23 *************************/
-        ActivityCompat.requestPermissions(this, new String[] {
+        ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
         }, 1);
         /****************************************************************/
 
-        loginButton = (LoginButton)findViewById(R.id.button_login_facebook);
+        btnLoginFacebook = (LoginButton) findViewById(R.id.button_login_facebook);
+        btnLoginGoogle = (SignInButton) findViewById(R.id.button_login_google);
+
 
         callbackManager = CallbackManager.Factory.create();
-        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_friends"));
-        loginButton.setOnClickListener(this);
+        btnLoginFacebook.setReadPermissions(Arrays.asList("public_profile", "email", "user_friends"));
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, connectionResult -> {})
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Customizing G+ button
+        btnLoginGoogle.setSize(SignInButton.SIZE_STANDARD);
+        btnLoginGoogle.setScopes(gso.getScopeArray());
+
+        btnLoginFacebook.setOnClickListener(this);
+        btnLoginGoogle.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(LoginActivity.this, "Yo! Logged in!", Toast.LENGTH_SHORT).show();
-                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+        switch (view.getId()) {
+            case R.id.button_login_facebook:
+
+                btnLoginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        registerUserWithFacebook(object);
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(LoginActivity.this, "Yo! Logged in!", Toast.LENGTH_SHORT).show();
+                        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                                (object, response) -> registerUserWithFacebook(object));
+
+                        Bundle b = new Bundle();
+                        b.putString("fields", "id,email,first_name,last_name,picture.type(large)");
+                        request.setParameters(b);
+                        request.executeAsync();
                     }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+                    }
+
                 });
-                Bundle b = new Bundle();
-                b.putString("fields","id,email,first_name,last_name,picture.type(large)");
-                request.setParameters(b);
-                request.executeAsync();
-            }
+                break;
 
-            @Override
-            public void onCancel() {
-                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
-            }
-
-        });
+            case R.id.button_login_google:
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+                break;
+        }
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            registerUserWithGoogle(result);
+        }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+
     private void registerUserWithFacebook(JSONObject user) {
         try {
-            user_email = user.getString("email");
-            fb_id = user.getString("id");
-            name = user.getString("first_name") + " " + user.getString("last_name");
+            userEmail = user.getString("email");
+            userID = user.getString("id");
+            userName = user.getString("first_name") + " " + user.getString("last_name");
             try {
-                URL picture = new URL("https://graph.facebook.com/" + fb_id + "/picture?type=large");
-                path_to_pic = picture.toString();
+                URL picture = new URL("https://graph.facebook.com/" + userID + "/picture?type=large");
+                userPicture = picture.toString();
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -114,18 +158,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Toast.makeText(LoginActivity.this, e.toString(), Toast.LENGTH_LONG).show();
         }
 
-//        if (path_to_pic != null) {
-//            File profile = new File(getApplicationContext().getFilesDir().getPath(), "profile.jpg");
-//        }
-
         SessionManager session = SessionManager.getInstance(getApplicationContext());
-        session.createUserLoginSession(name, user_email, fb_id, path_to_pic);
+        session.createUserLoginSession(userName, userEmail, userID, userPicture, "facebook");
 
         startActivity(new Intent(this, SplashActivity.class));
-        Log.i(TAG, "onCompleted: user_name  : " + name);
-        Log.i(TAG, "onCompleted: user_email : " + user_email);
-        Log.i(TAG, "onCompleted: facebook_id: " + fb_id);
-        Log.i(TAG, "onCompleted: path_to_pic: " + path_to_pic);
+        Log.i(TAG, "onCompleted: userName    : " + userName);
+        Log.i(TAG, "onCompleted: userEmail   : " + userEmail);
+        Log.i(TAG, "onCompleted: userID      : " + userID);
+        Log.i(TAG, "onCompleted: userPicture : " + userPicture);
+    }
+
+    private void registerUserWithGoogle(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            if (acct != null ) {
+                userPicture = (acct.getPhotoUrl() != null) ? acct.getPhotoUrl().toString() : "";
+                userName = acct.getDisplayName();
+                userEmail = acct.getEmail();
+                userID = acct.getId();
+            }
+
+
+            SessionManager session = SessionManager.getInstance(getApplicationContext());
+            session.createUserLoginSession(userName, userEmail, userID, userPicture, "google");
+
+            startActivity(new Intent(this, SplashActivity.class));
+
+            Log.i(TAG, "onCompleted: userName    : " + userName);
+            Log.i(TAG, "onCompleted: userEmail   : " + userEmail);
+            Log.i(TAG, "onCompleted: userID      : " + userID);
+            Log.i(TAG, "onCompleted: userPicture : " + userPicture);
+
+        }
+
     }
 
 
